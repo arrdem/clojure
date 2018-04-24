4966ea93529161752b5f56877754ea4a97a24ead
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-04-24 00:30:07 -0700

    Tweak docstring

--- src/clj/clojure/core.clj
+++ src/clj/clojure/core.clj
@@ -5782,7 +5782,7 @@
 
 (defmacro defonce
   "defs name to have the root value of the expr iff the named var has no root value,
-  else expr is unevaluated"
+  else expr will not be evaluated"
   {:added "1.0"
    :arglists '([name doc? meta? expr])}
   [name & args]
